c18ff9c0318bdf8eec264f67318f0637fc0fe587
Julius Celik <julius.celik@digitalroute.com>
2019-08-12 14:39:12 +0200

    feat(response): responses gets support for raw schemas

--- lib/response.ts
+++ lib/response.ts
@@ -1,7 +1,29 @@
+import { JSONSchema7 } from 'json-schema';
+import { cleanObject } from './utils/clean-object';
+
+type InternalContent = {
+  mediaType: string;
+  schema: JSONSchema7;
+};
+
 type Internals = {
   httpCode: number;
   description?: string;
-}
+  content?: InternalContent;
+};
+
+type CompiledContent = {
+  [key: string]: {
+    schema: JSONSchema7;
+  };
+};
+
+type CompiledResponse = {
+  [httpCode: string]: {
+    description?: string;
+    content?: CompiledContent;
+  };
+};
 
 export class BaggerResponse {
   public readonly isBagger = true;
@@ -10,19 +32,39 @@ export class BaggerResponse {
   constructor(httpCode: number) {
     this.internals = {
       httpCode
-    }
+    };
   }
 
-  public description(description: string) {
+  public description(description: string): BaggerResponse {
     this.internals.description = description;
     return this;
   }
 
-  public compile() {
-    return ({
-      [this.internals.httpCode]: {
-        description: this.internals.description
+  public content(mediaType: string, schema: JSONSchema7): BaggerResponse {
+    this.internals.content = { mediaType, schema };
+    return this;
+  }
+
+  private static compileContent(content: InternalContent | undefined): CompiledContent | undefined {
+    if (content === undefined) {
+      return undefined;
+    }
+    const { mediaType, schema } = content;
+    return {
+      [mediaType]: {
+        schema
       }
-    })
+    };
+  }
+
+  public compile(): CompiledResponse {
+    const compiledPolluted = {
+      [this.internals.httpCode]: {
+        description: this.internals.description,
+        content: BaggerResponse.compileContent(this.internals.content)
+      }
+    };
+
+    return cleanObject(compiledPolluted);
   }
 }
--- test/response.test.ts
+++ test/response.test.ts
@@ -1,17 +1,33 @@
-import test from 'ava'
-import bagger from '../lib/bagger'
+import test from 'ava';
+import bagger from '../lib/bagger';
+import { JSONSchema7 } from 'json-schema';
 
 test('Creating a bagger response will return isBagger = true', t => {
-    const response = bagger.response(200)
-    t.is(response.isBagger, true)
-})
+  const response = bagger.response(200);
+  t.is(response.isBagger, true);
+});
 
 test('Simple response can be compiled', t => {
-    const response = bagger.response(200).description('Successfully fetched request')
+  const response = bagger.response(200).description('Successfully fetched request');
 
-    t.deepEqual(response.compile(), {
-        200: {
-            description: 'Successfully fetched request'
+  t.snapshot(response.compile());
+});
+
+test('Responses could have contents that have a specified type', t => {
+  const schema: JSONSchema7 = {
+    type: 'string',
+    examples: ['foo']
+  };
+
+  const response = bagger.response(200).content('application/json', schema);
+
+  t.deepEqual(response.compile(), {
+    200: {
+      content: {
+        'application/json': {
+          schema
         }
-    })
-})
+      }
+    }
+  });
+});
