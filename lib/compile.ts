1fe74386c38ff8112bcf5a119878f5608ef51587
Julius Celik <julius.celik@digitalroute.com>
2019-08-13 14:36:56 +0200

    docs: Add JS Docs

--- lib/compile.ts
+++ lib/compile.ts
@@ -22,6 +22,12 @@ function compileRequests(requests: BaggerRequest[]): PathsObject {
   return {};
 }
 
+/**
+ * Compiles all requests into a Swagger definition.
+ * @param configuration General configuration settings describing the API.
+ * @param requests All requests that together describe the API.
+ * @param components Any additional components that are externally defined.
+ */
 export function compile(
   configuration: SwaggerConfiguration,
   requests: BaggerRequest[],
