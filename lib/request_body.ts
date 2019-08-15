1cae43c2b0d73915ce8f04027823c806330e4c70
Julius Celik <julius.celik@digitalroute.com>
2019-08-15 16:08:24 +0200

    docs: add jsdocs in `request_body.ts`

--- lib/request_body.ts
+++ lib/request_body.ts
@@ -10,22 +10,38 @@ export class BaggerRequestBody {
   private _content: Content = new Content();
   private schema?: Schema;
 
+  /**
+   * Set the description of the request body. It can be markdown-formatted.
+   * @param description The description of the request body.
+   */
   public description(description: string): BaggerRequestBody {
     this._description = description;
     return this;
   }
 
+  /**
+   * Request bodies are optional by default. To mark the body as required, use required: `true`.
+   * @param required if the request body is required.
+   */
   public required(required: boolean): BaggerRequestBody {
     this._required = required;
     return this;
   }
 
+  /**
+   * Content describes the content of a body.
+   * @param contentType The media type of the content. Like 'application/json' or 'text/plain'. See https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types for more information
+   * @param schema A `hapi/joi` schema describing the content of the body. This can also be used for validating requests in run time by using `.getSchema()`.
+   */
   public content(contentType: string, schema: Schema): BaggerRequestBody {
     this.schema = schema;
     this._content.add(contentType, schema);
     return this;
   }
 
+  /**
+   * Get the `hapi/joi` schema that is used to describe the body content. It can be used for validating requests in runtime.
+   */
   public getSchema(): Schema {
     if (!this.schema) {
       throw new BaggerUndefinedSchemaError();
@@ -33,6 +49,9 @@ export class BaggerRequestBody {
     return this.schema;
   }
 
+  /**
+   * Creates a compiled object representation of the request body.
+   */
   public compile(): RequestBodyObject {
     return {
       description: this._description,
