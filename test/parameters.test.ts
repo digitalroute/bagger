615971fd1ac50b60da4984960a15223bc2f1ddcd
Tommy Gunnarsson <tommy.gunnarsson@digitalroute.com>
2019-08-16 15:05:15 +0200

    feat: make it possible to fetch schema from parameter

---
+++ test/parameters.test.ts
@@ -0,0 +1,16 @@
+import * as joi from '@hapi/joi';
+import { BaggerParameter } from '../lib/parameters';
+
+describe('Parameters', () => {
+  test('Add path parameter with schema', () => {
+    const schema = joi.string().valid(['backpack', 'duffel']);
+    const parameter = new BaggerParameter('path', 'bagType');
+    parameter.schema(schema);
+    expect(parameter.compile()).toMatchSnapshot();
+    const actualSchema = parameter.getSchemas();
+    const expectedSchema = {
+      'application/json': schema
+    };
+    expect(actualSchema).toEqual(expectedSchema);
+  });
+});
