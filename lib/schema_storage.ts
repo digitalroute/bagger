46ae9736efdff4b0bc3de03d2fe571afbd01026b
Tommy Gunnarsson <tommy.gunnarsson@digitalroute.com>
2019-08-19 09:31:58 +0200

    feat: concatenate joi schema for parameters

--- lib/schema_storage.ts
+++ lib/schema_storage.ts
@@ -3,12 +3,19 @@ import { ContentSchemas } from './content';
 
 export class BaggerSchemaDoesNotExistForKeyError extends Error {}
 
+export class BaggerCannotAddParmeterSchemaWithoutNameError extends Error {}
+
+export class BaggerCannotResolveSwaggerKeyError extends Error {}
+
+interface KeyToSchema {
+  [key: string]: Schema;
+}
 export interface SchemaDefinition {
   body?: Schema;
-  query?: Schema;
-  header?: Schema;
-  path?: Schema;
-  cookie?: Schema;
+  query?: KeyToSchema;
+  headers?: KeyToSchema;
+  params?: KeyToSchema;
+  cookies?: KeyToSchema;
 }
 
 export interface PathSchema {
@@ -17,23 +24,54 @@ export interface PathSchema {
 interface RequestToSchema {
   [key: string]: PathSchema;
 }
+
+type SwaggerLocationTypes = 'cookie' | 'header' | 'path' | 'body' | 'query';
+
 class SchemaStorage {
+  private readonly swaggerKeyToJoiKey = {
+    cookie: 'cookies',
+    header: 'headers',
+    path: 'params',
+    body: 'body',
+    query: 'query'
+  };
   private requestToSchema: RequestToSchema = {};
 
   private buildKey(path: string, method: string): string {
     return `${method.toUpperCase()}_${path.toUpperCase()}`;
   }
 
-  public addRequestSchemas(path: string, method: string, schemas: ContentSchemas, type: keyof SchemaDefinition): void {
+  private toJoiType = (key: SwaggerLocationTypes): keyof SchemaDefinition => {
+    const type = this.swaggerKeyToJoiKey[key] as keyof SchemaDefinition;
+    if (!type) {
+      throw new BaggerCannotResolveSwaggerKeyError();
+    }
+    return type;
+  } 
+
+  public addRequestSchemas(path: string, method: string, schemas: ContentSchemas, type: SwaggerLocationTypes, parameterName?: string): void {
     const key = this.buildKey(path, method);
     if (!this.requestToSchema[key]) {
       this.requestToSchema[key] = {};
     }
+    const joiType = this.toJoiType(type);
     Object.keys(schemas).forEach((contentType: string): void => {
       if (!this.requestToSchema[key][contentType]) {
         this.requestToSchema[key][contentType] = {};
       }
-      this.requestToSchema[key][contentType][type] = schemas[contentType];
+      if (joiType !== 'body') {
+        if (!parameterName) {
+          throw new BaggerCannotAddParmeterSchemaWithoutNameError();
+        }
+
+        const params = this.requestToSchema[key][contentType][joiType] || {};
+        this.requestToSchema[key][contentType][joiType] = {
+          ...params,
+          [parameterName]: schemas[contentType]
+        };
+      } else {
+        this.requestToSchema[key][contentType][joiType] = schemas[contentType];
+      }
     });
   }
 
