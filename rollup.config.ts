cac6a296bf5ab204e6b5c76643a3825c7c2157ce
Márcio Saeger <marcio.saeger@digitalroute.com>
2022-02-15 10:47:52 +0100

    Update rollup.config.ts
    Co-authored-by: Tommy Gunnarsson <46932544+tgun89@users.noreply.github.com>

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json';
 
 const pkg = require('./package.json');
 
-const libraryName = 'index';
+const libEntrypoint = 'index';
 
 export default {
   input: `lib/${libraryName}.ts`,
